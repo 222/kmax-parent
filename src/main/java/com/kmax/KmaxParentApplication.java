678303764df74f0da528e2e8a9652cdf0f585ec7
rocks <652630339@qq.com>
2024-12-02 18:08:34 +0800

    pom commit

---
+++ src/main/java/com/kmax/KmaxParentApplication.java
@@ -0,0 +1,13 @@
+package com.kmax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KmaxParentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KmaxParentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kmax/KmaxParentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kmax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KmaxParentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
